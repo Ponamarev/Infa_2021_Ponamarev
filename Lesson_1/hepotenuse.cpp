7d32eb920898fa77d22df42ef4ddf511a14f83ab
evgeniy <Ponamarev>
2021-02-01 14:39:22 +0300

    Added two exsersises.

---
+++ Lesson_1/hepotenuse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+
+    float katet_1;
+    float katet_2;
+
+    std:: cin >> katet_1 >> katet_2;
+    float hepotenuse = sqrt(katet_1 * katet_1 + katet_2 * katet_2);
+    std:: cout << hepotenuse << std::endl;
+
+    return 0;
+}
