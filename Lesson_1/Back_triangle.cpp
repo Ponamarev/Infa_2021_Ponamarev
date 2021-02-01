fffac796c081dc90d149db70cd3b9ccd97878f0d
evgeniy <Ponamarev>
2021-02-01 15:27:40 +0300

    Added exersise.

---
+++ Lesson_1/Back_triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+
+    int lenght_rect;
+
+    std::cin >> lenght_rect;
+    for (int i = 0; i <= lenght_rect; i++){
+        for (int g = 0; g <= lenght_rect - i; g++){
+            std::cout << '*';
+        }
+        std:: cout << std::endl;
+    }
+
+    return 0;
+}
+
