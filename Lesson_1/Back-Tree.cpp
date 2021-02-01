786af38c641ff9dbfe586cfc991a03c81a716fcc
evgeniy <Ponamarev>
2021-02-01 19:53:37 +0300

    Add exsersise.

---
+++ Lesson_1/Back-Tree.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+
+    int lenght_rect, len;
+
+    std::cin >> lenght_rect;
+    for (int i = lenght_rect/2; i >= 0; i--){
+        for (int g = 0; g <= lenght_rect; g++){
+            len = (lenght_rect - 2*i) / 2;
+            if (g >= len and g < lenght_rect - len){
+                std::cout << '*';
+            }
+            else{
+                std::cout << ' ';
+            }
+        }
+        std:: cout << std::endl;
+    }
+
+    return 0;
+}
